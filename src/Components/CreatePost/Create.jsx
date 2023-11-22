bb588c440252473c48b6fbf787c360e117c26f56
Mihail Zaykov <mixail_zaikov@abv.bg>
2023-11-22 17:19:11 +0200

    Create Posts Service

--- src/Components/CreatePost/Create.jsx
+++ src/Components/CreatePost/Create.jsx
@@ -1,8 +1,20 @@
+
+
 function Create() {
+  const createPostSubmitHandler = (e) => {
+    e.preventDefault();
+
+    const postData = Object.fromEntries(new FormData(e.currentTarget))
+
+    console.log(postData);
+  }
+
   return (
     <div className="flex justify-center items-center h-screen bg-black bg-opacity-90 ">
-      <form className="w-1/2 dark:bg-gray-800 p-8 rounded-lg">
-        <h1 className="text-center text-3xl font-bold text-white pb-8">Add review</h1>
+      <form className="w-1/2 dark:bg-gray-800 p-8 rounded-lg" onSubmit={createPostSubmitHandler}>
+        <h1 className="text-center text-3xl font-bold text-white pb-8">
+          Add review
+        </h1>
         <div className="mb-6">
           <label
             htmlFor="movie"
@@ -13,6 +25,7 @@ function Create() {
           <input
             type="text"
             id="title"
+            name="title"
             className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-indigo-500 focus:border-indigo-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-indigo-500 dark:focus:border-indigo-500"
             required=""
           />
@@ -27,6 +40,7 @@ function Create() {
           <input
             type="text"
             id="year"
+            name="year"
             className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-indigo-500 focus:border-indigo-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-indigo-500 dark:focus:border-indigo-500"
             required=""
           />
@@ -41,6 +55,7 @@ function Create() {
           <input
             type="text"
             id="genre"
+            name="genre"
             className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-indigo-500 focus:border-indigo-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-indigo-500 dark:focus:border-indigo-500"
             required=""
           />
@@ -55,20 +70,22 @@ function Create() {
           <input
             type="text"
             id="imageUrl"
+            name="imageUrl"
             className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-indigo-500 focus:border-indigo-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-indigo-500 dark:focus:border-indigo-500"
             required=""
           />
         </div>
         <div className="mb-6">
           <label
-            htmlFor="revie"
+            htmlFor="review"
             className="block mb-2 text-sm font-medium text-gray-900 dark:text-white"
           >
-           Review:
+            Review:
           </label>
           <textarea
             type="text"
             id="review"
+            name="review"
             className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-indigo-500 focus:border-indigo-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-indigo-500 dark:focus:border-indigo-500"
             required=""
           />
