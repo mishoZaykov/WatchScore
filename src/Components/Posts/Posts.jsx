3167defe0d92ae6862f3478c77af78ab106c8ce4
Mihail Zaykov <mixail_zaikov@abv.bg>
2023-11-23 18:27:14 +0200

    Added styling to the posts.

--- src/Components/Posts/Posts.jsx
+++ src/Components/Posts/Posts.jsx
@@ -6,24 +6,22 @@ import * as movieService from "../../services/movieService";
 import PostItem from "./PostItem/PostItem";
 
 function Posts() {
-  const [movies, setMovies] = useState([])
-  
+  const [movies, setMovies] = useState([]);
+
   useEffect(() => {
-    movieService.getAll()
-      .then(result => setMovies(result))
-  }, [])
+    movieService.getAll().then((result) => setMovies(result));
+  }, []);
 
   return (
-    <section>
-      <h1>All Movie Posts</h1>
+    <section className="flex flex-col justify-center items-center">
+      <h1 className="text-center text-4xl m-5 font-bold">All Movie Posts</h1>
+      <div >
+        {movies.map((movie) => (
+          <PostItem  key={movie._id} {...movie} />
+        ))}
+      </div>
 
-      {movies.map(movie => (
-        <PostItem key={movie._id} {...movie}/>
-      ))}
-
-      {movies.length === 0 && (
-        <h1>No posts yet</h1>
-      )}
+      {movies.length === 0 && <h1>No posts yet</h1>}
     </section>
   );
 }
