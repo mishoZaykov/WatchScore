0fa392f0311f9df340e151be709823ca16630bde
Mihail Zaykov <mixail_zaikov@abv.bg>
2023-11-27 11:04:04 +0200

    Migrated to collections

--- src/services/movieService.js
+++ src/services/movieService.js
@@ -1,17 +1,18 @@
 import * as request from "../lib/request";
 
-const baseUrl = "http://localhost:3030/jsonstore/movies";
+const baseUrl = "http://localhost:3030/data/movies";
 
 export const getAll = async () => {
   const result = await request.get(baseUrl);
 
-  return Object.values(result);
+  return result;
 };
 
 export const getOne = async (movieId) => {
-  const result = await request.get(`${baseUrl}/${movieId}`);
+  const result = await request.get(`${baseUrl}/${movieId}`,);
+  
   return result;
-}
+};
 
 export const create = async (movieData) => {
   const result = await request.post(baseUrl, movieData);
