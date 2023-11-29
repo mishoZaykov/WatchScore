a8587092f753feb2bf7b0a76441dcd21cf3f1b3c
Mihail Zaykov <mixail_zaikov@abv.bg>
2023-11-29 13:36:12 +0200

    Show edit and delete buttons on owner

--- src/Components/Details/Details.jsx
+++ src/Components/Details/Details.jsx
@@ -1,20 +1,26 @@
-import { useContext, useEffect, useState } from "react";
+import { useContext, useEffect, useReducer, useState } from "react";
 import { useParams } from "react-router-dom";
 
 import * as movieService from "../../services/movieService";
 import * as commentService from "../../services/commentService";
 import AuthContext from "../../context/authContext";
+import reducer from "./commentReducer";
 
 function Details() {
-  const { username } = useContext(AuthContext);
+  const { username, userId } = useContext(AuthContext);
   const [movie, setMovie] = useState({});
-  const [comments, setComments] = useState([]);
+  const [comments, dispatchComment] = useReducer(reducer, []);
   const { movieId } = useParams();
 
   useEffect(() => {
     movieService.getOne(movieId).then(setMovie);
 
-    commentService.getAll(movieId).then(setComments);
+    commentService.getAll(movieId).then((result) => {
+      dispatchComment({
+        type: "GET_ALL_COMMENTS",
+        payload: result,
+      });
+    });
   }, [movieId]);
 
   const addCommentHandler = async (e) => {
@@ -27,7 +33,12 @@ function Details() {
       formData.get("comment")
     );
 
-    setComments((state) => [...state, { ...newComment, author: { username } }]);
+    newComment.owner = { username };
+
+    dispatchComment({
+      type: "ADD_COMMENT",
+      payload: newComment,
+    });
   };
 
   return (
@@ -59,75 +70,22 @@ function Details() {
               <h3 className="text-gray-900 text-3xl title-font font-medium mb-5">
                 {movie.genre}
               </h3>
-              {/* <div className="flex mb-4">
-                <span className="flex items-center">
-                  <svg
-                    fill="currentColor"
-                    stroke="currentColor"
-                    strokeLinecap="round"
-                    strokeLinejoin="round"
-                    strokeWidth={2}
-                    className="w-4 h-4 text-red-500"
-                    viewBox="0 0 24 24"
-                  >
-                    <path d="M12 2l3.09 6.26L22 9.27l-5 4.87 1.18 6.88L12 17.77l-6.18 3.25L7 14.14 2 9.27l6.91-1.01L12 2z" />
-                  </svg>
-                  <svg
-                    fill="currentColor"
-                    stroke="currentColor"
-                    strokeLinecap="round"
-                    strokeLinejoin="round"
-                    strokeWidth={2}
-                    className="w-4 h-4 text-red-500"
-                    viewBox="0 0 24 24"
-                  >
-                    <path d="M12 2l3.09 6.26L22 9.27l-5 4.87 1.18 6.88L12 17.77l-6.18 3.25L7 14.14 2 9.27l6.91-1.01L12 2z" />
-                  </svg>
-                  <svg
-                    fill="currentColor"
-                    stroke="currentColor"
-                    strokeLinecap="round"
-                    strokeLinejoin="round"
-                    strokeWidth={2}
-                    className="w-4 h-4 text-red-500"
-                    viewBox="0 0 24 24"
-                  >
-                    <path d="M12 2l3.09 6.26L22 9.27l-5 4.87 1.18 6.88L12 17.77l-6.18 3.25L7 14.14 2 9.27l6.91-1.01L12 2z" />
-                  </svg>
-                  <svg
-                    fill="currentColor"
-                    stroke="currentColor"
-                    strokeLinecap="round"
-                    strokeLinejoin="round"
-                    strokeWidth={2}
-                    className="w-4 h-4 text-red-500"
-                    viewBox="0 0 24 24"
-                  >
-                    <path d="M12 2l3.09 6.26L22 9.27l-5 4.87 1.18 6.88L12 17.77l-6.18 3.25L7 14.14 2 9.27l6.91-1.01L12 2z" />
-                  </svg>
-                  <svg
-                    fill="none"
-                    stroke="currentColor"
-                    strokeLinecap="round"
-                    strokeLinejoin="round"
-                    strokeWidth={2}
-                    className="w-4 h-4 text-red-500"
-                    viewBox="0 0 24 24"
-                  >
-                    <path d="M12 2l3.09 6.26L22 9.27l-5 4.87 1.18 6.88L12 17.77l-6.18 3.25L7 14.14 2 9.27l6.91-1.01L12 2z" />
-                  </svg>
-                  <span className="text-gray-600 ml-3">4 Reviews</span>
-                </span>
-              </div> */}
+
               <h2 className="text-sm title-font text-gray-500 tracking-widest">
                 REVIEW:
               </h2>
               <p className="leading-relaxed">{movie.review}</p>
-              <div className="flex">
-                <button className="flex ml-auto text-white bg-red-500 border-0 py-2 px-6 focus:outline-none hover:bg-red-600 rounded">
-                  Button
-                </button>
-              </div>
+
+              {userId === movie._ownerId && (
+                <div className="flex justify-end gap-3">
+                  <button className="flex  text-white bg-indigo-700 border-0 py-2 px-8 focus:outline-none hover:bg-indigo-700 rounded">
+                    Edit
+                  </button>
+                  <button className="flex  text-white bg-indigo-700 border-0 py-2 px-8 focus:outline-none hover:bg-indigo-700 rounded">
+                    Delete
+                  </button>
+                </div>
+              )}
 
               <div className="antialiased mx-auto max-w-screen-sm">
                 <h3 className="mb-4 text-lg font-semibold text-gray-900">
